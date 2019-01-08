41e10a00b5985c20fcf1220f44307a68e5893b68
Sasha <sasha1rus@gmail.com>
2019-01-07 18:02:18 -0800

    rustfmt (mostly changing to hard tabs)

--- src/main.rs
+++ src/main.rs
@@ -12,33 +12,33 @@ use rand::{FromEntropy, Rng};
 use state::GameState;
 
 fn main() {
-    let mut rng = XorShiftRng::from_entropy();
-    let mut state = GameState::new(2);
-    while !state.is_finished {
-        println!("player {}", state.current_player);
+	let mut rng = XorShiftRng::from_entropy();
+	let mut state = GameState::new(2);
+	while !state.is_finished {
+		println!("player {}", state.current_player);
 
-        let actions = state.available_actions().clone();
-        println!("  turn state: {}", state.turn_state);
-        println!(
-            "  available actions: {}",
-            if actions.is_empty() { "none" } else { "" }
-        );
-        for action in actions.clone() {
-            println!("    {}", action);
-        }
-        let action = rng.choose(&actions).unwrap().clone();
-        println!("  action: {}", action);
-        println!();
-        state.do_action(action);
-    }
+		let actions = state.available_actions().clone();
+		println!("  turn state: {}", state.turn_state);
+		println!(
+			"  available actions: {}",
+			if actions.is_empty() { "none" } else { "" }
+		);
+		for action in actions.clone() {
+			println!("    {}", action);
+		}
+		let action = rng.choose(&actions).unwrap().clone();
+		println!("  action: {}", action);
+		println!();
+		state.do_action(action);
+	}
 
-    if state.winner.is_some() {
-        let winner = state.winner.unwrap();
-        println!("winner: {}", winner,);
-    } else {
-        println!("no winner!");
-    }
-    for (index, score) in state.player_scores.iter().enumerate() {
-        println!("player {} with {} points", index, score);
-    }
+	if state.winner.is_some() {
+		let winner = state.winner.unwrap();
+		println!("winner: {}", winner,);
+	} else {
+		println!("no winner!");
+	}
+	for (index, score) in state.player_scores.iter().enumerate() {
+		println!("player {} with {} points", index, score);
+	}
 }
--- src/types.rs
+++ src/types.rs
@@ -8,82 +8,90 @@ pub type CardPile = Vec<Card>;
 
 #[derive(Clone, Copy, PartialEq)]
 pub enum Color {
-    Yellow,
-    Green,
-    White,
-    Red,
-    Blue,
+	Yellow,
+	Green,
+	White,
+	Red,
+	Blue,
 }
 impl Display for Color {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        let string = match self {
-            Color::Yellow => "Yellow",
-            Color::Green => "Green",
-            Color::White => "White",
-            Color::Red => "Red",
-            Color::Blue => "Blue",
-        };
-        f.write_str(string)
-    }
+	fn fmt(&self, f: &mut Formatter) -> Result {
+		let string = match self {
+			Color::Yellow => "Yellow",
+			Color::Green => "Green",
+			Color::White => "White",
+			Color::Red => "Red",
+			Color::Blue => "Blue",
+		};
+		f.write_str(string)
+	}
 }
 
 #[derive(Clone, Copy, PartialEq)]
 pub struct Card {
-    pub color: Color,
-    pub number: i8,
+	pub color: Color,
+	pub number: i8,
 }
 impl Card {
-    pub fn new(color: Color, number: i8) -> Card {
-        Card { color, number }
-    }
+	pub fn new(color: Color, number: i8) -> Card {
+		Card { color, number }
+	}
 }
 impl Display for Card {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "Card({} {})", self.color, self.number)
-    }
+	fn fmt(&self, f: &mut Formatter) -> Result {
+		write!(f, "Card({} {})", self.color, self.number)
+	}
 }
 
 #[derive(Clone, Copy, PartialEq)]
 pub enum PawnType {
-    Adventurer,
-    Researcher,
+	Adventurer,
+	Researcher,
 }
 impl Display for PawnType {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        let string = match self {
-            PawnType::Adventurer => "Adventurer",
-            PawnType::Researcher => "Researcher",
-        };
-        f.write_str(string)
-    }
+	fn fmt(&self, f: &mut Formatter) -> Result {
+		let string = match self {
+			PawnType::Adventurer => "Adventurer",
+			PawnType::Researcher => "Researcher",
+		};
+		f.write_str(string)
+	}
 }
 
 #[derive(Clone, Copy, PartialEq)]
 pub enum Event {
-    Points(Score),
-    Artifact,
-    Arrow,
+	Points(Score),
+	Artifact,
+	Arrow,
 }
 
 #[derive(Clone, PartialEq)]
 pub enum Action {
-    PlayCard(Card),
-    DiscardCard(Card),
-    DrawFromDeck,
-    DrawFromDiscard(Color),
-    ChoosePawnType(PawnType),
-    MoveColor(Color),
+	PlayCard(Card),
+	DiscardCard(Card),
+	DrawFromDeck,
+	DrawFromDiscard(Color),
+	ChoosePawnType(PawnType),
+	MoveColor(Color),
 }
 impl Display for Action {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        let string = match self {
-            Action::PlayCard(card) => format!("PlayCard({} {})", card.color, card.number),
-            Action::DiscardCard(card) => format!("DiscardCard({} {})", card.color, card.number),
-            Action::DrawFromDeck => String::from("DrawFromDeck"),
-            Action::DrawFromDiscard(color) => format!("DrawFromDiscard({})", color),
-            Action::ChoosePawnType(pawn_type) => format!("ChoosePawnType({})", pawn_type),
-            Action::MoveColor(color) => format!("MoveColor({})", color),
-        };
-        f.write_str(&*string)
-    }
+	fn fmt(&self, f: &mut Formatter) -> Result {
+		let string = match self {
+			Action::PlayCard(card) => {
+				format!("PlayCard({} {})", card.color, card.number)
+			}
+			Action::DiscardCard(card) => {
+				format!("DiscardCard({} {})", card.color, card.number)
+			}
+			Action::DrawFromDeck => String::from("DrawFromDeck"),
+			Action::DrawFromDiscard(color) => {
+				format!("DrawFromDiscard({})", color)
+			}
+			Action::ChoosePawnType(pawn_type) => {
+				format!("ChoosePawnType({})", pawn_type)
+			}
+			Action::MoveColor(color) => format!("MoveColor({})", color),
+		};
+		f.write_str(&*string)
+	}
 }
